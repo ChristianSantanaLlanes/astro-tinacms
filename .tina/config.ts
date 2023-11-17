df71fad41d7b6ec8e350998ecd902b9e220538fb
Dylan Awalt-Conley <web@dylan.ac>
2023-11-17 15:15:38 -0500

    minimal reproduction of tinacms/tinacms#4345
    the problem has to do with the order of hydration; logging this to the
    console.

--- .tina/config.ts
+++ .tina/config.ts
@@ -1,7 +1,90 @@
-import { defineStaticConfig } from "tinacms";
+import { defineStaticConfig, type Collection } from "tinacms";
 
 // Your hosting provider likely exposes this as an environment variable
 const branch = process.env.HEAD || process.env.VERCEL_GIT_COMMIT_REF || "main";
+
+const globalData: Collection = {
+  label: "Global Data",
+  name: "global",
+  path: "content/page",
+  match: {
+    include: "global",
+  },
+  format: "yaml",
+  fields: [
+    {
+      name: "title",
+      label: "Test Content",
+      type: "string",
+    },
+  ],
+  ui: {
+    allowedActions: {
+      create: false,
+      delete: false,
+    },
+  },
+};
+
+const home: Collection = {
+  label: "Home Page",
+  name: "home",
+  path: "content/page",
+  match: {
+    include: "home",
+  },
+  format: "mdx",
+  fields: [
+    {
+      name: "body",
+      label: "Main Content",
+      type: "rich-text",
+      isBody: true,
+    },
+  ],
+  ui: {
+    allowedActions: {
+      create: false,
+      delete: false,
+    },
+    router: ({ document }) => {
+      if (document._sys.filename === "home") {
+        return `/`;
+      }
+      return undefined;
+    },
+  },
+};
+
+const about: Collection = {
+  label: "About Page",
+  name: "about",
+  path: "content/page",
+  match: {
+    include: "about",
+  },
+  format: "mdx",
+  fields: [
+    {
+      name: "body",
+      label: "Main Content",
+      type: "rich-text",
+      isBody: true,
+    },
+  ],
+  ui: {
+    allowedActions: {
+      create: false,
+      delete: false,
+    },
+    router: ({ document }) => {
+      if (document._sys.filename === "about") {
+        return `/about`;
+      }
+      return undefined;
+    },
+  },
+};
 
 export default defineStaticConfig({
   branch,
@@ -18,34 +101,6 @@ export default defineStaticConfig({
     },
   },
   schema: {
-    collections: [
-      {
-        name: "post",
-        label: "Posts",
-        path: "content/posts",
-        ui: {
-          allowedActions: {
-            create: false,
-            delete: false,
-          },
-          router: () => '/',
-        },
-        fields: [
-          {
-            type: "string",
-            name: "title",
-            label: "Title",
-            required: true,
-            isTitle: true,
-          },
-          {
-            type: "rich-text",
-            name: "body",
-            label: "Body",
-            isBody: true,
-          },
-        ],
-      },
-    ],
+    collections: [globalData, home, about],
   },
 });
--- src/components/react.tsx
+++ src/components/react.tsx
@@ -2,10 +2,11 @@ import { useTina } from "tinacms/dist/react";
 
 export const MyComponent = (props) => {
   const { data } = useTina(props);
+  console.log("hydrating", data);
 
   return (
     <pre style={{ margin: "100px auto", width: "1000px" }}>
-      {JSON.stringify(data?.post?.body, null, 2)}
+      {JSON.stringify(data, null, 2)}
     </pre>
   );
 };
